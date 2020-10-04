234d246c22faead4d09a1aa2d1c4781c6bb6289e
lamarricane <froggsycopp@gmail.com>
2020-10-04 13:52:22 +0400

    Добавление расширяюего класса UnitFunction

---
+++ src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/functions/UnitFunction.java
@@ -0,0 +1,8 @@
+package ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions;
+
+public class UnitFunction extends ConstantFunction {
+
+    public UnitFunction() {
+        super(1); 
+    }
+}
