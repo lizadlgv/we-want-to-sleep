e3e1ccef8031882fc7764e8700a896b4561042a3
lamarricane <froggsycopp@gmail.com>
2020-10-04 12:07:22 +0400

    Добавление класса DivisionFunction

---
+++ src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/functions/DivisionFunction.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions;
+
+public class DivisionFunction implements MathFunction {
+    @Override
+    public double apply(double x) {
+        return x/2.0;
+    }
+}
+
