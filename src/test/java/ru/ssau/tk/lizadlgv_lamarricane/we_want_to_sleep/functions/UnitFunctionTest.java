e525e3375242fdce7fb16afb2067f68056b23a61
lamarricane <froggsycopp@gmail.com>
2020-10-04 13:53:08 +0400

    Покрытие класса UnitFunction тестами

---
+++ src/test/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/functions/UnitFunctionTest.java
@@ -0,0 +1,14 @@
+package ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions;
+
+import static org.testng.Assert.*;
+
+import org.testng.annotations.Test;
+
+public class UnitFunctionTest {
+    UnitFunction testFunction = new UnitFunction();
+
+    @Test
+    public void testApply() {
+        assertEquals(testFunction.getConstant(), 1.0);
+    }
+}
