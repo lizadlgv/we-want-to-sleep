e5c10f6606a8b42f840e48ee735b55f0eea37bc3
lamarricane <froggsycopp@gmail.com>
2020-10-07 21:47:07 +0400

    Добавлен SoftAssert

--- src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/functions/ConstantFunction.java
+++ src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/functions/ConstantFunction.java
@@ -2,6 +2,7 @@ package ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions;
 
 public class ConstantFunction implements MathFunction {
     private final double constant;
+
     public double getConstant() {
         return constant;
     }
--- src/test/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/functions/MathFunctionTest.java
+++ src/test/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/functions/MathFunctionTest.java
@@ -1,10 +1,12 @@
 package ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions;
 
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import static org.testng.Assert.*;
 
 public class MathFunctionTest {
+
     private final MathFunction identity = new IdentityFunction();
     private final MathFunction root = new FourthRootFunction();
     private final MathFunction constant = new ConstantFunction(3);
@@ -15,9 +17,14 @@ public class MathFunctionTest {
 
     @Test
     public void testAndThen() {
-        assertEquals(composite.apply(4), 2);
-        assertEquals(division.andThen(sqr).apply(16), 64);
-        assertEquals(root.andThen(constant).apply(7), 3);
-        assertEquals(unit.andThen(division).andThen(identity).apply(67), 0.5);
+
+        SoftAssert softAssert = new SoftAssert();
+
+        softAssert.assertEquals(composite.apply(4), 2.0);
+        softAssert.assertEquals(division.andThen(sqr).apply(16), 64.0);
+        softAssert.assertEquals(root.andThen(constant).apply(7), 3.0);
+        softAssert.assertEquals(unit.andThen(division).andThen(identity).apply(67), 0.5);
+
+        softAssert.assertAll();
     }
 }
