4d67854c23df458dda284191c740ebe10bc8454e
lamarricane <froggsycopp@gmail.com>
2020-10-07 21:17:55 +0400

    Покрытие тестами MathFunction

---
+++ src/test/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/functions/MathFunctionTest.java
@@ -0,0 +1,23 @@
+package ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class MathFunctionTest {
+    private final MathFunction identity = new IdentityFunction();
+    private final MathFunction root = new FourthRootFunction();
+    private final MathFunction constant = new ConstantFunction(3);
+    private final MathFunction division = new DivisionFunction();
+    private final MathFunction sqr = new SqrFunction();
+    private final MathFunction unit = new UnitFunction();
+    private final MathFunction composite = identity.andThen(sqr).andThen(root);
+
+    @Test
+    public void testAndThen() {
+        assertEquals(composite.apply(4), 2);
+        assertEquals(division.andThen(sqr).apply(16), 64);
+        assertEquals(root.andThen(constant).apply(7), 3);
+        assertEquals(unit.andThen(division).andThen(identity).apply(67), 0.5);
+    }
+}
