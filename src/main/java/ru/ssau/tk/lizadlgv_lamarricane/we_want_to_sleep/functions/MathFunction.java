3138b736bc89c75798d4f590c00b25a6fc8c652f
lamarricane <froggsycopp@gmail.com>
2020-10-07 19:59:15 +0400

    Изменения в интерфейсе

--- src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/functions/MathFunction.java
+++ src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/functions/MathFunction.java
@@ -2,4 +2,8 @@ package ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions;
 
 public interface MathFunction {
     double apply(double x);
+
+    default CompositeFunction andThen(MathFunction afterFunction) {
+        return new CompositeFunction(this, afterFunction);
+    }
 }
