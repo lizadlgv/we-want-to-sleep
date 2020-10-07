e39493fc9267a973964f7f410493bc997a5e4fbc
lamarricane <froggsycopp@gmail.com>
2020-10-07 20:51:11 +0400

    Исправлен SqrFunctionTest

---
+++ src/test/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/functions/SqrFunctionTest.java
@@ -0,0 +1,18 @@
+package ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions;
+
+import static org.testng.Assert.*;
+
+import org.testng.annotations.Test;
+
+public class SqrFunctionTest {
+
+    @Test
+    public void testApply() {
+        SqrFunction testFunction = new SqrFunction();
+        assertEquals (testFunction.apply(5), 25);
+        assertEquals (testFunction.apply(-11), 121);
+        assertEquals (testFunction.apply(-1), 1);
+        assertEquals (testFunction.apply(0.1), 0.01, 0.01);
+
+    }
+}
