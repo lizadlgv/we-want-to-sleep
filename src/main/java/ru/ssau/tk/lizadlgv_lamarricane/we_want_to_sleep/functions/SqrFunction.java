4faa58b53115f9945877b84999f5e2a064b48791
lizadlgv <liza.dolgova2002@mail.ru>
2020-10-08 19:48:52 +0400

    Удалён лишний импорт из SqrFunction

--- src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/functions/SqrFunction.java
+++ src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/functions/SqrFunction.java
@@ -1,7 +1,5 @@
 package ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions;
 
-import java.lang.Math;
-
 public class SqrFunction implements MathFunction {
     @Override
     public double apply(double x) {
