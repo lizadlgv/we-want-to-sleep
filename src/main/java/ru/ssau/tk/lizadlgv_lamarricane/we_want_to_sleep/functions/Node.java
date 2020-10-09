85cc7d842a0b54e3ca5a7af215f5f834951191c4
lizadlgv <liza.dolgova2002@mail.ru>
2020-10-09 23:51:14 +0400

    Добавлен класс Node

---
+++ src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/functions/Node.java
@@ -0,0 +1,8 @@
+package ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions;
+
+public class Node {
+    public Node next;
+    public Node prev;
+    public double x;
+    public double y;
+}
