0345949c1823e5292d47c552e55b2b4188f6b3fe
lizadlgv <liza.dolgova2002@mail.ru>
2020-10-24 16:06:10 +0400

    Покрытие тестами LinkedListTabulatedFunction, но пока что не работает тест метода extrapolateRight()

---
+++ src/test/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/functions/LinkedListTabulatedFunctionTest.java
@@ -0,0 +1,123 @@
+package ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions;
+
+import org.testng.annotations.Test;
+
+import org.testng.asserts.SoftAssert;
+
+public class LinkedListTabulatedFunctionTest {
+
+    private final static double DELTA = 0.0001;
+
+    private final double[] valuesX = new double[]{0., 1., 2., 3., 4.};
+    private final double[] valuesY = new double[]{0., 1., 4., 9., 16.};
+
+    private final MathFunction sqr = new SqrFunction();
+
+    public LinkedListTabulatedFunction sampleFunction() {
+        return new LinkedListTabulatedFunction(valuesX, valuesY);
+    }
+
+    public LinkedListTabulatedFunction sampleSqr() {
+        return new LinkedListTabulatedFunction(sqr, 0., 4., 5);
+    }
+
+    SoftAssert softAssert = new SoftAssert();
+
+    @Test
+    public void testGetCount() {
+        softAssert.assertEquals(sampleFunction().getCount(), 5, DELTA);
+        softAssert.assertEquals(sampleSqr().getCount(), 5, DELTA);
+        softAssert.assertAll();
+    }
+
+    @Test
+    public void testLeftBound() {
+        softAssert.assertEquals(sampleFunction().leftBound(), 0., DELTA);
+        softAssert.assertEquals(sampleSqr().leftBound(), 0., DELTA);
+        softAssert.assertAll();
+    }
+
+    @Test
+    public void testRightBound() {
+        softAssert.assertEquals(sampleFunction().rightBound(), 4., DELTA);
+        softAssert.assertEquals(sampleSqr().rightBound(), 4., DELTA);
+        softAssert.assertAll();
+    }
+
+    @Test
+    public void testGetX() {
+        softAssert.assertEquals(sampleFunction().getX(2), 2., DELTA);
+        softAssert.assertEquals(sampleSqr().getX(1), 1., DELTA);
+        softAssert.assertAll();
+    }
+
+    @Test
+    public void testGetY() {
+        softAssert.assertEquals(sampleFunction().getY(1), 1., DELTA);
+        softAssert.assertEquals(sampleSqr().getY(2), 4., DELTA);
+        softAssert.assertAll();
+    }
+
+    @Test
+    public void testSetY() {
+        LinkedListTabulatedFunction testSample = new LinkedListTabulatedFunction(valuesX, valuesY);
+        LinkedListTabulatedFunction testSampleSqr = new LinkedListTabulatedFunction(sqr, 0., 4., 5);
+
+        testSample.setY(1, 42.);
+        testSampleSqr.setY(1, 42.);
+
+        softAssert.assertEquals(testSample.getY(1), 42., DELTA);
+        softAssert.assertEquals(testSampleSqr.getY(1), 42., DELTA);
+        softAssert.assertAll();
+    }
+
+    @Test
+    public void testIndexOfX() {
+        softAssert.assertEquals(sampleFunction().indexOfX(4.), 4, DELTA);
+        softAssert.assertEquals(sampleSqr().indexOfX(4.), 4, DELTA);
+        softAssert.assertEquals(sampleFunction().indexOfX(42.), -1, DELTA);
+        softAssert.assertEquals(sampleSqr().indexOfX(42.), -1, DELTA);
+        softAssert.assertAll();
+    }
+
+    @Test
+    public void testIndexOfY() {
+        softAssert.assertEquals(sampleFunction().indexOfY(16.), 4, DELTA);
+        softAssert.assertEquals(sampleSqr().indexOfY(16.), 4, DELTA);
+        softAssert.assertEquals(sampleFunction().indexOfY(42.), -1, DELTA);
+        softAssert.assertEquals(sampleSqr().indexOfY(42.), -1, DELTA);
+        softAssert.assertAll();
+    }
+
+    @Test
+    public void testFloorIndexOfX() {
+        softAssert.assertEquals(sampleFunction().floorIndexOfX(3.5), 3, DELTA);
+        softAssert.assertEquals(sampleSqr().floorIndexOfX(16.), 4, DELTA);
+        softAssert.assertEquals(sampleFunction().floorIndexOfX(-1.), 0, DELTA);
+        softAssert.assertEquals(sampleSqr().floorIndexOfX(0), 0, DELTA);
+        softAssert.assertAll();
+    }
+
+    @Test
+    public void testInterpolate() {
+        softAssert.assertEquals(sampleFunction().interpolate(2., 2), 4., DELTA);
+        softAssert.assertEquals(sampleSqr().interpolate(2.5, 4), 10., DELTA);
+        softAssert.assertEquals(sampleFunction().interpolate(0, 0), 0., DELTA);
+        softAssert.assertEquals(sampleSqr().interpolate(4., 2), 14., DELTA);
+        softAssert.assertAll();
+    }
+
+    @Test
+    public void testExtrapolateLeft() {
+        softAssert.assertEquals(sampleFunction().extrapolateLeft(-47.), -47., DELTA);
+        softAssert.assertEquals(sampleSqr().extrapolateLeft(-1.), -1., DELTA);
+        softAssert.assertAll();
+    }
+
+    @Test
+    public void testExtrapolateRight() {
+        softAssert.assertEquals(sampleFunction().extrapolateRight(5.), 23., DELTA);
+        softAssert.assertEquals(sampleSqr().extrapolateRight(9.), 51., DELTA);
+        softAssert.assertAll();
+    }
+}
