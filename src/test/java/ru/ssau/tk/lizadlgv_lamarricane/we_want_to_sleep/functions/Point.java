b16486d73d04f5a0a8ec1d5b4278e093fd856339
lizadlgv <liza.dolgova2002@mail.ru>
2020-11-15 19:56:26 +0400

    Создан класс Point

---
+++ src/test/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/functions/Point.java
@@ -0,0 +1,11 @@
+package ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions;
+
+public class Point {
+    final public double x;
+    final public double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
