4bbc5cad91b5debf94ad1d509a039293e6b45735
lamarricane <froggsycopp@gmail.com>
2020-11-21 08:51:24 +0400

    3.1* Создание класса UnmodifiableTabulatedFunction, реализующего интерфейс TabulatedFunction

---
+++ src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/functions/UnmodifiableTabulatedFunction.java
@@ -0,0 +1,68 @@
+package ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions;
+
+import java.util.Iterator;
+
+public class UnmodifiableTabulatedFunction implements TabulatedFunction {
+    private TabulatedFunction function;
+
+    public UnmodifiableTabulatedFunction(TabulatedFunction function) {
+        this.function = function;
+    }
+
+    @Override
+    public int getCount() {
+
+        return function.getCount();
+    }
+
+    @Override
+    public double getX(int index) {
+
+        return function.getX(index);
+    }
+
+    @Override
+    public double getY(int index) {
+
+        return function.getY(index);
+    }
+
+    @Override
+    public void setY(int index, double value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int indexOfX(double x) {
+
+        return function.indexOfX(x);
+    }
+
+    @Override
+    public int indexOfY(double y) {
+
+        return function.indexOfY(y);
+    }
+
+    @Override
+    public double leftBound() {
+        return function.leftBound();
+    }
+
+    @Override
+    public double rightBound() {
+
+        return function.rightBound();
+    }
+
+    @Override
+    public Iterator<Point> iterator() {
+
+        return function.iterator();
+    }
+
+    @Override
+    public double apply(double x) {
+        return function.apply(x);
+        }
+}
