227b1f6e5a8effd7610392d2b84bf9bc2a0ce451
lizadlgv <liza.dolgova2002@mail.ru>
2020-11-21 20:54:35 +0400

    3.1 X* и Y* Тесты точно закончены

--- src/test/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/functions/UnmodifiableTabulatedFunctionTest.java
+++ src/test/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/functions/UnmodifiableTabulatedFunctionTest.java
@@ -19,18 +19,21 @@ public class UnmodifiableTabulatedFunctionTest {
     public void testGetCount() {
         assertEquals(array.getCount(), 3);
         assertEquals(list.getCount(), 4);
+        assertEquals(unmodifiableInStrict.getCount(), 3);
     }
 
     @Test
     public void testGetX() {
         assertEquals(array.getX(1), 6., DELTA);
         assertEquals(list.getX(3), 13., DELTA);
+        assertEquals(unmodifiableInStrict.getX(0), -1., DELTA);
     }
 
     @Test
     public void testGetY() {
         assertEquals(array.getY(1), 3., DELTA);
         assertEquals(list.getY(3), 11., DELTA);
+        assertEquals(unmodifiableInStrict.getY(2), 10., DELTA);
     }
 
     @Test
@@ -44,30 +47,35 @@ public class UnmodifiableTabulatedFunctionTest {
     public void testIndexOfX() {
         assertEquals(array.indexOfX(3.), -1);
         assertEquals(list.indexOfX(1.), -1);
+        assertEquals(unmodifiableInStrict.indexOfX(-1), 0);
     }
 
     @Test
     public void testIndexOfY() {
         assertEquals(array.indexOfY(9.), -1);
         assertEquals(list.indexOfY(16.), -1);
+        assertEquals(unmodifiableInStrict.indexOfY(3.), 1);
     }
 
     @Test
     public void testLeftBound() {
         assertEquals(array.leftBound(), -1., DELTA);
         assertEquals(list.leftBound(), -1., DELTA);
+        assertEquals(unmodifiableInStrict.leftBound(), -1., DELTA);
     }
 
     @Test
     public void testRightBound() {
         assertEquals(array.rightBound(), 9., DELTA);
         assertEquals(list.rightBound(), 13., DELTA);
+        assertEquals(unmodifiableInStrict.rightBound(), 9., DELTA);
     }
 
     @Test
     public void testIterator() {
         assertEquals(array.iterator().next().x, -1., DELTA);
         assertEquals(list.iterator().next().x, -1., DELTA);
+        assertEquals(unmodifiableInStrict.iterator().next().x, -1., DELTA);
     }
 
     @Test
@@ -76,5 +84,6 @@ public class UnmodifiableTabulatedFunctionTest {
         assertEquals(array.apply(2), 0.7142, DELTA);
         assertEquals(list.apply(-3), -2.1428, DELTA);
         assertEquals(list.apply(5), 2.4285, DELTA);
+        assertEquals(unmodifiableInStrict.apply(6.), 3., DELTA);
     }
 }
