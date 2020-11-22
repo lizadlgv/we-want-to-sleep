95fc283894885122b460e3cef58fcd96489d4752
lizadlgv <liza.dolgova2002@mail.ru>
2020-11-22 21:23:11 +0400

    Заметка для Маши по оформлению кода: 1) после открытия фигурных скобок(после объявления класса\метода, не важно) пустой строки быть не должно(пустые строки ставятся после объявления переменных, между методами, между логическими участками кода внутри метода), 2) смотри, чтобы после всего написанного кода не было пустых строк, далее ошибки не контренто из этого класса, но были в других: 3) методы должны быть расположены в логическом убывании(сверху всегда геттеры и сеттеры, а далее по усложнению и логике), 4) всегда проверяй что тебе пишут в warning, у тебя не редко встречаются лишние импорты

--- src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/operations/TabulatedFunctionOperationService.java
+++ src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/operations/TabulatedFunctionOperationService.java
@@ -1,11 +1,10 @@
 package ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.operations;
 
-import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.*;
+import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.Point;
+import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.TabulatedFunction;
 
 public class TabulatedFunctionOperationService {
-
     public static Point[] asPoints(TabulatedFunction tabulatedFunction) {
-
         int i = 0;
         Point[] points = new Point[tabulatedFunction.getCount()];
         for (Point point : tabulatedFunction) {
