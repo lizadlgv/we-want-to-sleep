199c368ea0a63a153c7eff72fc450747eb5d82bf
lamarricane <froggsycopp@gmail.com>
2020-11-25 07:11:23 +0400

    3.2 тесты

---
+++ src/test/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/operations/TabulatedFunctionOperationServiceTest.java
@@ -0,0 +1,39 @@
+package ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.operations;
+
+import org.testng.annotations.Test;
+import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.factory.*;
+import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.ArrayTabulatedFunction;
+import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.LinkedListTabulatedFunction;
+import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.Point;
+import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.TabulatedFunction;
+import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.factory.LinkedListTabulatedFunctionFactory;
+
+import static org.testng.Assert.*;
+
+public class TabulatedFunctionOperationServiceTest {
+    private final static double DELTA = 0.0001;
+    private final double[] valuesX = new double[]{-27, -8, -1, 0, 1, 8, 27};
+    private final double[] valuesY = new double[]{-3, -2, -1, -0, 1, 2, 3};
+
+    private TabulatedFunction testArrayFunction = new ArrayTabulatedFunction(valuesX, valuesY);
+    private TabulatedFunction testListFunction = new LinkedListTabulatedFunction(valuesX, valuesY);
+
+    @Test
+    public void testAsPoints() {
+        Point[] Points = TabulatedFunctionOperationService.asPoints(testArrayFunction);
+        int i = 0;
+        for (Point myPoint : Points) {
+            assertEquals(myPoint.x, testArrayFunction.getX(i), DELTA);
+            assertEquals(myPoint.y, testArrayFunction.getY(i++), DELTA);
+        }
+        assertEquals(testArrayFunction.getCount(), i);
+
+        Points = TabulatedFunctionOperationService.asPoints(testListFunction);
+        i = 0;
+        for (Point myPoint : Points) {
+            assertEquals(myPoint.x, testListFunction.getX(i), DELTA);
+            assertEquals(myPoint.y, testListFunction.getY(i++), DELTA);
+        }
+        assertEquals(testListFunction.getCount(), i);
+    }
+}
