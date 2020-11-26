da9b935da225e18f55898ce5c26744a990e5e84e
lamarricane <froggsycopp@gmail.com>
2020-11-26 21:31:10 +0400

    3.2 очередное исключение

---
+++ src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/exceptions/InconsistentFunctionsException.java
@@ -0,0 +1,10 @@
+package ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.exceptions;
+
+public class InconsistentFunctionsException extends RuntimeException {
+    public InconsistentFunctionsException() {
+    }
+
+    public InconsistentFunctionsException(String message) {
+        super(message);
+    }
+}
