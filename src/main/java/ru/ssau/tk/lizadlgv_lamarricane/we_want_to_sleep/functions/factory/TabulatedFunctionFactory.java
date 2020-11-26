633e13f37a8833409ee9df65ec4830060c1c1663
lamarricane <froggsycopp@gmail.com>
2020-11-27 01:14:30 +0400

    🦈🦈🦈

--- src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/functions/factory/TabulatedFunctionFactory.java
+++ src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/functions/factory/TabulatedFunctionFactory.java
@@ -4,14 +4,16 @@ import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.StrictTabulate
 import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.TabulatedFunction;
 import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.UnmodifiableTabulatedFunction;
 
-public interface TabulatedFunctionFactory {
+public interface TabulatedFunctionFactory<createUnmodifiable> {
     TabulatedFunction create(double[] xValues, double[] yValues);
 
     default TabulatedFunction createStrict(double[] xValues, double[] yValues) {
         return new StrictTabulatedFunction(create(xValues, yValues));
     }
 
-//    Маш, вставь createUnmodifiable на место этого комментария, а комментарий удали
+    default TabulatedFunction createUnmodifiable(double[] xValues, double[] yValues) {
+        return new UnmodifiableTabulatedFunction(create(xValues, yValues));
+    }
 
     default TabulatedFunction createStrictUnmodifiable(double[] xValues, double[] yValues) {
         return new UnmodifiableTabulatedFunction(new StrictTabulatedFunction(create(xValues, yValues)));
