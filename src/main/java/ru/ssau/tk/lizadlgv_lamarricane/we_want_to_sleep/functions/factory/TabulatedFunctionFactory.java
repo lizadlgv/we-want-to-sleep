ad93a4f2b8675a49152e32f2a7cefb716d937d0e
lizadlgv <liza.dolgova2002@mail.ru>
2020-11-26 22:40:26 +0400

    3.2 X* В TabulatedFunctionFactory добавлен и протестирован метод createStrict()

--- src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/functions/factory/TabulatedFunctionFactory.java
+++ src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/functions/factory/TabulatedFunctionFactory.java
@@ -1,7 +1,12 @@
 package ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.factory;
 
+import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.StrictTabulatedFunction;
 import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.TabulatedFunction;
 
 public interface TabulatedFunctionFactory {
     TabulatedFunction create(double[] xValues, double[] yValues);
+
+    default TabulatedFunction createStrict(double[] xValues, double[] yValues) {
+        return new StrictTabulatedFunction(create(xValues, yValues));
+    }
 }
