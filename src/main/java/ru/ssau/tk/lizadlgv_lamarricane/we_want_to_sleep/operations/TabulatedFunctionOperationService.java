3fc273826ff3cc30216ac9b6adb02b1da6ddf6e9
lamarricane <froggsycopp@gmail.com>
2020-11-26 23:57:44 +0400

    3.2 пункт 2

--- src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/operations/TabulatedFunctionOperationService.java
+++ src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/operations/TabulatedFunctionOperationService.java
@@ -1,5 +1,6 @@
 package ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.operations;
 
+import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.exceptions.InconsistentFunctionsException;
 import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.Point;
 import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.TabulatedFunction;
 import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.factory.ArrayTabulatedFunctionFactory;
@@ -32,5 +33,34 @@ public class TabulatedFunctionOperationService {
             points[i++] = point;
         }
         return points;
+    }
+    private interface BiOperation {
+        double apply(double u, double v);
+    }
+    private TabulatedFunction doOperation(TabulatedFunction a, TabulatedFunction b, BiOperation operation) {
+        if (a.getCount() != b.getCount()) {
+            throw new InconsistentFunctionsException("Количество записей в первой табулированной функции не совпадает с количеством записей во второй");
+        }
+        Point[] aPoints = asPoints(a);
+        Point[] bPoints = asPoints(b);
+        double[] xValues = new double[a.getCount()];
+        double[] yValues = new double[b.getCount()];
+
+        for (int i = 0; i < a.getCount(); i++) {
+            if (aPoints[i].x != bPoints[i].x) {
+                throw new InconsistentFunctionsException("Координаты x двух функций не совпадают.");
+            }
+            xValues[i] = aPoints[i].x;
+            yValues[i] = operation.apply(aPoints[i].y, bPoints[i].y);
+        }
+        return factory.create(xValues, yValues);
+    }
+
+    public TabulatedFunction sum(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, Double::sum);
+    }
+
+    public TabulatedFunction subtract(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u, v) -> u - v);
     }
 }
