a6e25b83ac0484abd094483caf04926ee4a1cddf
lamarricane <froggsycopp@gmail.com>
2020-11-27 00:34:32 +0400

    3.2 работа с абстрактным классом SteppingDifferentialOperator

---
+++ src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/operations/SteppingDifferentialOperator.java
@@ -0,0 +1,23 @@
+package ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.operations;
+
+import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.MathFunction;
+
+public abstract class SteppingDifferentialOperator implements DifferentialOperator<MathFunction> {
+    protected double step;
+
+    public SteppingDifferentialOperator(double step) {
+        this.step = step;
+        if (step <= 0 || step == Double.POSITIVE_INFINITY || Double.isNaN(step)) {
+            throw new IllegalArgumentException("Step не соответствует условию");
+        }
+        this.step = step;
+    }
+
+    public double getStep() {
+        return step;
+    }
+
+    public void setStep(double step) {
+        this.step = step;
+    }
+}
