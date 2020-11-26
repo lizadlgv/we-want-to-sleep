19411daa5f0eecf08065dc8bdf6a5fce04bfa034
lamarricane <froggsycopp@gmail.com>
2020-11-26 21:33:25 +0400

    3.2 добавление конструкторов

--- src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/operations/TabulatedFunctionOperationService.java
+++ src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/operations/TabulatedFunctionOperationService.java
@@ -2,8 +2,21 @@ package ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.operations;
 
 import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.Point;
 import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.TabulatedFunction;
+import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.factory.ArrayTabulatedFunctionFactory;
+import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.factory.TabulatedFunctionFactory;
 
 public class TabulatedFunctionOperationService {
+
+    private TabulatedFunctionFactory factory;
+
+    public TabulatedFunctionOperationService(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
+    public TabulatedFunctionOperationService() {
+        factory = new ArrayTabulatedFunctionFactory();
+    }
+
     public static Point[] asPoints(TabulatedFunction tabulatedFunction) {
         int i = 0;
         Point[] points = new Point[tabulatedFunction.getCount()];
