139e56803069c701d5d9f4be511a6a2725f17dd6
lamarricane <froggsycopp@gmail.com>
2020-12-06 17:05:13 +0400

    3.3 добавление пакета io и завершенного класса FunctionsIO

---
+++ src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/io/FunctionsIO.java
@@ -0,0 +1,7 @@
+package ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.io;
+
+public final class FunctionsIO {
+    private FunctionsIO() {
+        throw new UnsupportedOperationException();
+    }
+}
