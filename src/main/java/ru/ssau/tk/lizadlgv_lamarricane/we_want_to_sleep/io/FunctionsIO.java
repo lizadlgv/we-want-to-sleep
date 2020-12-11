a0510a7906407e5bcd506398e638a45dee9c4512
lizadlgv <liza.dolgova2002@mail.ru>
2020-12-11 22:31:50 +0400

    мы замучались уже, но это снова FunctionsIO

--- src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/io/FunctionsIO.java
@@ -4,6 +4,9 @@ import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.*;
 import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.factory.*;
 
 import java.io.*;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
 
 public final class FunctionsIO {
     private FunctionsIO() {
@@ -19,4 +22,43 @@ public final class FunctionsIO {
         writer.flush();
     }
 
+    public static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
+        DataOutputStream out = new DataOutputStream(outputStream);
+        out.writeInt(function.getCount());
+        for (Point newPoint : function) {
+            out.writeDouble(newPoint.x);
+            out.writeDouble(newPoint.y);
+        }
+        out.flush();
+    }
+
+    public static TabulatedFunction readTabulatedFunction(BufferedReader reader, TabulatedFunctionFactory factory) throws IOException {
+        int count = Integer.parseInt(reader.readLine());
+        double[] xValues = new double[count];
+        double[] yValues = new double[count];
+        NumberFormat formatter = NumberFormat.getInstance(Locale.forLanguageTag("ru"));
+        for (int i = 0; i < count; i++) {
+            String tempString = reader.readLine();
+            try {
+                xValues[i] = formatter.parse(tempString.split(" ")[0]).doubleValue();
+                yValues[i] = formatter.parse(tempString.split(" ")[1]).doubleValue();
+            } catch (ParseException pe) {
+                throw new IOException(pe);
+            }
+        }
+        return factory.create(xValues, yValues);
+    }
+
+    public static TabulatedFunction readTabulatedFunction(BufferedInputStream inputStream, TabulatedFunctionFactory
+            factory) throws IOException {
+        DataInputStream in = new DataInputStream(inputStream);
+        int length = in.readInt();
+        double[] xValues = new double[length];
+        double[] yValues = new double[length];
+        for (int i = 0; i < length; i++) {
+            xValues[i] = in.readDouble();
+            yValues[i] = in.readDouble();
+        }
+        return factory.create(xValues, yValues);
+    }
 }
