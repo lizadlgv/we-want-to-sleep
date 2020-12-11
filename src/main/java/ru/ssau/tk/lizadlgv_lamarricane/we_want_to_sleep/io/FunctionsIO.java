e146e5345b6ae80f8a58f6276482d4fa1d8c065c
lizadlgv <liza.dolgova2002@mail.ru>
2020-12-11 21:25:59 +0400

    3.3 правки

--- src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/io/FunctionsIO.java
@@ -1,7 +1,31 @@
 package ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.io;
 
+import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.Point;
+import ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.functions.TabulatedFunction;
+
+import java.io.*;
+
 public final class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException();
+    }
+
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) throws IOException {
+        PrintWriter printWriter = new PrintWriter(writer);
+        printWriter.println(function.getCount());
+        for (Point point : function) {
+            printWriter.printf("%f %f\n", point.x, point.y);
+        }
+        writer.flush();
+    }
+
+    static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
+        DataOutputStream out = new DataOutputStream(outputStream);
+        out.writeInt(function.getCount());
+        for (Point newPoint : function) {
+            out.writeDouble(newPoint.x);
+            out.writeDouble(newPoint.y);
+        }
+        out.flush();
     }
 }
