7b6ec89c1ad5a35ffec61a9ea8c9174bf2bbe199
lizadlgv <liza.dolgova2002@mail.ru>
2020-12-11 23:09:50 +0400

    3.3 X Добавлен метод десериализции

--- src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/io/FunctionsIO.java
@@ -61,4 +61,8 @@ public final class FunctionsIO {
         }
         return factory.create(xValues, yValues);
     }
+
+    public static TabulatedFunction deserialize(BufferedInputStream stream) throws IOException, ClassNotFoundException {
+        return (TabulatedFunction) new ObjectInputStream(stream).readObject();
+    }
 }
