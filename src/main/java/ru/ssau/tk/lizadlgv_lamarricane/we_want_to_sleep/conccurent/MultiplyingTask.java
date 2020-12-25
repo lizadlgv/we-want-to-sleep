012dc1755b0c274959fa7f7016348673ac6fdc03
lamarricane <froggsycopp@gmail.com>
2020-12-25 22:47:28 +0400

    ...

--- src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/conccurent/MultiplyingTask.java
+++
@@ -1,4 +0,0 @@
-package ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.conccurent;
-
-public class MultiplyingTask {
-}
---
+++ src/main/java/ru/ssau/tk/lizadlgv_lamarricane/we_want_to_sleep/concurrent/MultiplyingTask.java
@@ -0,0 +1,4 @@
+package ru.ssau.tk.lizadlgv_lamarricane.we_want_to_sleep.concurrent;
+
+public class MultiplyingTask {
+}
